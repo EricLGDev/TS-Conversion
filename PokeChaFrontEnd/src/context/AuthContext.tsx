3dc817a72078f65d3a3eca12337fc5fb44bf17df
Eric <ericlg.link@gmail.com>
2023-02-02 02:41:36 -0800

    Fixed context for now

--- PokeChaFrontEnd/src/context/AuthContext.tsx
+++ PokeChaFrontEnd/src/context/AuthContext.tsx
@@ -1,34 +1,53 @@
-import { createContext, useReducer, useEffect } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react';
 
-export const AuthContext = createContext()
+interface State {
+  user: object | null;
+}
 
-export const authReducer = (state, action) => {
+interface Action {
+  type: string;
+  payload: object;
+}
+
+interface AuthContextProps {
+  state: State;
+  dispatch: React.Dispatch<Action>;
+}
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+export const authReducer = (state: State, action: Action) => {
   switch (action.type) {
     case 'LOGIN':
-      return { user: action.payload }
+      return { user: action.payload };
     case 'LOGOUT':
-      return { user: null }
+      return { user: null };
     default:
-      return state
+      return state;
   }
+};
+
+interface Props {
+  children: React.ReactNode;
 }
 
-export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { 
-    user: null
-  })
+export const AuthContextProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
 
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'))
+    const user = JSON.parse(localStorage.getItem('user') || '');
     if (user) {
-      dispatch({ type: 'LOGIN', payload: user }) 
+      dispatch({ type: 'LOGIN', payload: user });
     }
-  }, [])
-  
-  return (
-    <AuthContext.Provider value={{ ...state, dispatch }}>
-      { children }
-    </AuthContext.Provider>
-  )
+  }, []);
 
-}
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
--- PokeChaFrontEnd/src/context/CardsContext.tsx
+++ PokeChaFrontEnd/src/context/CardsContext.tsx
@@ -1,54 +1,55 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, ReactNode } from 'react';
 
-interface CardAction {
-  type: string,
-  payload: any
+interface Card {
+  _id: string;
 }
 
-interface CardsState {
-  cards: any[]
+interface State {
+  cards: Card[] | null;
 }
 
-const initialState: CardsState = {
-  cards: []
-}
-
-const CardsContext = createContext<{ state: CardsState, dispatch: React.Dispatch<CardAction> }>({
-  state: initialState,
-  dispatch: () => null
-})
-
-const cardsReducer = (state: CardsState, action: CardAction) => {
-  switch (action.type) {
-    case 'SET_CARDS': 
-      return {
-        cards: action.payload
-      }
-    case 'CREATE_CARDS':
-      return {
-        cards: [action.payload, ...state.cards]
-      }
-    case 'DELETE_CARD':
-      return {
-        cards: state.cards.filter((w) => w._id !== action.payload._id)
-      }
-    default:
-      return state
-  }
+interface Action {
+  type: 'SET_CARDS' | 'CREATE_CARDS' | 'DELETE_CARD';
+  payload: any;
 }
 
 interface Props {
-  children: React.ReactNode
+  children?: ReactNode
+  // any props that come into the component
 }
 
-const CardsContextProvider = ({ children }: Props) => {
-  const [state, dispatch] = useReducer(cardsReducer, initialState)
+export const CardsContext = createContext<{ state: State; dispatch: React.Dispatch<Action> } | undefined>(
+  undefined
+);
+
+export const cardsReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_CARDS':
+      return {
+        cards: action.payload,
+      };
+    case 'CREATE_CARDS':
+      return {
+        cards: [action.payload, ...state.cards!],
+      };
+    case 'DELETE_CARD':
+      return {
+        cards: state.cards!.filter((w) => w._id !== action.payload._id),
+      };
+    default:
+      return state;
+  }
+};
+
+export const CardsContextProvider = ({ children }: Props) => {
+  const [state, dispatch] = useReducer(cardsReducer, {
+    cards: [],
+  });
 
   return (
     <CardsContext.Provider value={{ state, dispatch }}>
-      { children }
+      {children}
     </CardsContext.Provider>
-  )
-}
+  );
+};
 
-export { CardsContext, CardsContextProvider }
